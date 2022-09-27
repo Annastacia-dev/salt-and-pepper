f2246b690b16ce760da0d3685a26cad6305cddd2
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-09-27 19:14:28 +0300

    navbar & styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className='navbar'>
         <NavLink id='logo'>Salt &amp; Pepper</NavLink>
-        <NavLink><input type='text' placeholder="I'm looking for ..."></input></NavLink>
+        <NavLink><input type='text' placeholder="I'm searching for ..."></input></NavLink>
         <button><BiSearch/></button>
         <NavLink>Home</NavLink>
         <NavLink>Favourites</NavLink>
